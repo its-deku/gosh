014c96481c9d6135e5dd77237b86dda296ec36ee
yash-601 <yp804959@gmail.com>
2025-12-25 02:41:25 +0530

    extend parser to accomodate redirect in commands

--- parser/shell_parse.go
+++ parser/shell_parse.go
@@ -14,9 +14,13 @@ func ShellParse(stream string) []logger.Job2 {
 	if strings.Contains(stream, "&") {
 		parts := strings.Split(stream, "&")
 		for i := 0; i < len(parts)-1; i++ {
+			parts[i] = sanitize(parts[i])
 			jobs = append(jobs, subParser(parts[i], true))
 		}
-		jobs = append(jobs, subParser(parts[len(parts)-1], false))
+		if string(stream[len(stream)-1]) != "&" {
+			parts[len(parts)-1] = sanitize(parts[len(parts)-1])
+			jobs = append(jobs, subParser(parts[len(parts)-1], false))
+		}
 	} else {
 		jobs = append(jobs, subParser(stream, false))
 	}
@@ -24,19 +28,54 @@ func ShellParse(stream string) []logger.Job2 {
 	return jobs
 }
 
+func parseRedirect(cmd string) (string, string) {
+	var op string
+
+	if strings.Contains(cmd, ">") {
+		op = ">"
+	} else {
+		op = "$"
+	}
+
+	out := []string{}
+
+	for str := range strings.SplitSeq(cmd, op) {
+		out = append(out, str)
+	}
+
+	return strings.Join(out[:len(out)-1], " "), strings.Join(out[len(out)-1:], " ")
+}
+
 func subParser(stream string, bg bool) logger.Job2 {
 	pInd := 0
+	var arg, exec string
 	commands := []string{}
+	opt := []string{}
+	stream += " |"
+
 	for i, v := range stream {
+		arg = ""
 		if string(v) == "|" {
-			commands = append(commands, stream[pInd:i])
+			cmd := sanitize(stream[pInd:i])
+
+			// check for redirect operators in command
+			if strings.Contains(cmd, ">") || strings.Contains(cmd, "$") {
+				exec, arg = parseRedirect(cmd)
+				commands = append(commands, exec)
+			} else {
+				commands = append(commands, cmd)
+			}
+			opt = append(opt, arg)
+
 			pInd = i + 1
 		}
 	}
-	commands = append(commands, stream[pInd:])
+
+	// commands = append(commands, sanitize(stream[pInd:]))
 
 	return logger.Job2{
 		Background: bg,
 		Execs:      commands,
+		Opt:        opt,
 	}
 }
